9213c2900edce239f7e47768a20fb2799db25866
Michael Arango <michael.arango08@gmail.com>
2017-04-25 16:39:01 -0400

    Add SQL Queries

---
+++ SQL/ExampleWarehouseQueries.sql
@@ -0,0 +1,26 @@
+# select primary type & description
+SELECT `Primary Type`, Description
+FROM Chicago_Crime_Data_Warehouse.Crime_Description;
+# assaults
+SELECT * 
+FROM Chicago_Crime_Data_Warehouse.Crime_Description 
+WHERE `Primary Type` = "ASSAULT";
+# aggravated assualts with a handgun
+SELECT * 
+FROM Chicago_Crime_Data_Warehouse.Crime_Description 
+WHERE `Primary Type` = "ASSAULT" AND Description = "AGGRAVATED: HANDGUN";
+# assault or battery
+SELECT * 
+FROM Chicago_Crime_Data_Warehouse.Crime_Description 
+WHERE `Primary Type` = "ASSAULT" OR `Primary Type` = "Battery";
+# time queries
+SELECT Year, Month, Day
+FROM Chicago_Crime_Data_Warehouse.Time;
+# weekday
+SELECT Year, Month, Day
+FROM Chicago_Crime_Data_Warehouse.Time
+WHERE Weekday = "Saturday";
+# select and sort
+SELECT Year, Month, Day, Hour, Minute
+FROM Chicago_Crime_Data_Warehouse.Time
+ORDER BY Month, Day, Hour, Minute;
